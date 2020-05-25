b5cedb68f0150b2c4162ac495df3bba2064ea9a9
ymh <hanyongming@gmail.com>
2020-05-25 02:56:19 -0400

    Fixed typo

--- problems_chp_3_4/src/chp_3.jl
+++ problems_chp_3_4/src/chp_3.jl
@@ -8,7 +8,7 @@ p_grid = LinRange(0,1, 1000)
 ℙ_p = [1 for _ in 1:1000]
 
 bin_grid = [Binomial(9, p) for p in p_grid]
-ℙ_data = [pdf(bin, 6) for bin in bin_grid] # pdf(bin, 6) is f(6;p):=n_choose_6*p^6*(1-p)^{1-6}
+ℙ_data = [pdf(bin, 6) for bin in bin_grid] # pdf(bin, 6) is ℙ(6;p):=n_choose_6*p^6*(1-p)^{1-6}
 #histogram(ℙ_data)
 
 posterior = ℙ_data .* ℙ_p
